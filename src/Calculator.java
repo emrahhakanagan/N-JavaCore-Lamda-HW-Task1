b4e34948efac9faf6ed658f85b0ec0662d693457
emrahhakan <emrahhakanagan@gmail.com>
2024-01-28 22:41:33 +0300

    method divide is corrected

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,12 +6,8 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> divide = (x, y) -> {
-        if (y == 0) {
-            throw new ArithmeticException("Divide by zero !");
-        }
-        return x / y;
-    };
+
+    BinaryOperator<Integer> divide = (x, y) -> y == 0 ? 0 : x / y;
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
