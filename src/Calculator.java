c89f5292d035310641894e44b90124d13f995e67
emrahhakan <emrahhakanagan@gmail.com>
2024-01-28 10:25:07 +0300

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,23 @@
+import java.util.function.*;
+
+public class Calculator {
+
+    static Supplier<Calculator> instance = Calculator::new;
+
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> divide = (x, y) -> {
+        if (y == 0) {
+            throw new ArithmeticException("Divide by zero !");
+        }
+        return x / y;
+    };
+
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+
+    Consumer<Integer> println = System.out::println;
+
+    Predicate<Integer> isPissitive = x -> x > 0;
+}
---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.function.Consumer;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator calculator = Calculator.instance.get();
+
+        int a = calculator.plus.apply(1, 2);
+        int b = calculator.minus.apply(1, 1);
+//        int c = calculator.divide.apply(a, b);
+//        calculator.println.accept(c);
+
+        /*
+        * Проблема !!!
+        * Выводится ошибки нв консоль -->
+        * Exception in thread "main" java.lang.ArithmeticException: / by zero
+	    * at Calculator.lambda$new$3(Calculator.java:13)
+	    * at Main.main(Main.java:10)
+	    *
+        * Ошибка возникает при делении на ноль в выражении calc.divide.apply(a, b).
+        * Деление на ноль недопустимо в арифметике и создает исключению ArithmeticException.
+        *
+        * Решение -->
+        * Добавить проверку на ноль в операции деления в классе Calculator и
+        * выбросить исключение с сообщением, в случае деления равен нулю.
+        * В методе main обернуть вызов в блок try-catch для обработки возможного исключения.
+        *
+        * */
+
+
+
+
+
+        try {
+            int c = calculator.divide.apply(a, b); // Может возникнуть ArithmeticException
+            calculator.println.accept(c);
+        } catch (ArithmeticException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+
+
+    }
+
+}
