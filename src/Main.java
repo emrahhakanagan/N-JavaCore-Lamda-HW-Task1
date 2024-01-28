35a989ba099bc4070d3bd70eb184e00995689dc6
emrahhakan <emrahhakanagan@gmail.com>
2024-01-28 10:31:35 +0300

    problem has been found and resolved, also added coomit about resolving

--- src/Main.java
+++ src/Main.java
@@ -7,29 +7,6 @@ public class Main {
 
         int a = calculator.plus.apply(1, 2);
         int b = calculator.minus.apply(1, 1);
-//        int c = calculator.divide.apply(a, b);
-//        calculator.println.accept(c);
-
-        /*
-        * Проблема !!!
-        * Выводится ошибки нв консоль -->
-        * Exception in thread "main" java.lang.ArithmeticException: / by zero
-	    * at Calculator.lambda$new$3(Calculator.java:13)
-	    * at Main.main(Main.java:10)
-	    *
-        * Ошибка возникает при делении на ноль в выражении calc.divide.apply(a, b).
-        * Деление на ноль недопустимо в арифметике и создает исключению ArithmeticException.
-        *
-        * Решение -->
-        * Добавить проверку на ноль в операции деления в классе Calculator и
-        * выбросить исключение с сообщением, в случае деления равен нулю.
-        * В методе main обернуть вызов в блок try-catch для обработки возможного исключения.
-        *
-        * */
-
-
-
-
 
         try {
             int c = calculator.divide.apply(a, b); // Может возникнуть ArithmeticException
@@ -37,6 +14,24 @@ public class Main {
         } catch (ArithmeticException e) {
             System.out.println("Ошибка: " + e.getMessage());
         }
+
+
+        /*
+         * Проблема !!!
+         * Выводится ошибки нв консоль -->
+         * Exception in thread "main" java.lang.ArithmeticException: / by zero
+         * at Calculator.lambda$new$3(Calculator.java:13)
+         * at Main.main(Main.java:10)
+         *
+         * Ошибка возникает при делении на ноль в выражении calc.divide.apply(a, b).
+         * Деление на ноль недопустимо в арифметике и создает исключению ArithmeticException.
+         *
+         * Решение -->
+         * Добавить проверку на ноль в операции деления в классе Calculator и
+         * выбросить исключение с сообщением, в случае деления равен нулю.
+         * В методе main обернуть вызов в блок try-catch для обработки возможного исключения.
+         *
+         * */
 
 
     }
